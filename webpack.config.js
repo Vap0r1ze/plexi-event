1b995ff96f076c214ef69df72fbdf2c7832e461b
Vap0r1ze <superdash993@gmail.com>
2018-10-09 22:05:24 -0400

    add babel compiler

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,15 @@ module.exports = {
     rules: [{
       test: /\.scss$/,
       use: [ 'style-loader', 'css-loader', 'postcss-loader', 'sass-loader' ]
+    }, {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
     }]
   },
   output: {
